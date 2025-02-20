086d4cbb39a3898289647fec9b18eee230a31a32
veecodes14 <134261808+veecodes14@users.noreply.github.com>
2025-02-20 07:38:03 +0000

    Student tracker completed and committed

---
+++ student.py
@@ -0,0 +1,57 @@
+def student_records():
+    students = {}
+
+    while True:
+        print("\nWelcome to the Student Management System!")
+        print("1. Add new students")
+        print("2. Update existing student records")
+        print("3. Calculate average grade of all students")
+        print("4. Display all student records")
+
+        choice = input("Choose an option (1-4): ")
+
+        if choice == '1':
+            name = input("Enter name of student: ")
+            age = input("Enter age of student: ")
+            grade = input("Enter student's grade: ")
+            students[name] = {'age': age, 'grade':grade}
+            print(f"Student '{name}' added ")
+
+        elif choice == '2':
+            name = input("Enter name of student to update: ")
+            if name in students:
+                name = input("Enter new name: ")
+                age = input("Enter updated age: ")
+                grade = (input("Enter updated grade: ")).split()
+                students[name] = {'age': age, 'grade': grade}
+                print(f"Student: '{name}' updated")
+
+        elif choice == '3':
+            #def calculate_average(grade):
+                #return sum(grade) / len(grade)
+            
+            all_grades = []
+            for name, details in students.items():
+                all_grades.append(int(details['grade']))
+            average = sum(all_grades) / len(all_grades)
+            print(f"Average Grade: {average:.2f}")
+            
+            #for i in students:
+                #name, grade = students
+                #average = sum(grade) / len(grade)
+                #print(f"{name}: Average Grade: {average:.2f}")
+
+                #for student, grades in students:
+                #average = calculate_average(grades)
+                #print(f"{student}: Average Grade = {average:.2f}")
+
+        elif choice == '4':
+            if students:
+                print("\nStudent: ")
+                for name, info in students.items():
+                    print(f"Name: {name}, Age: {info['age']}, Grade: {info['grade']}")
+            else:
+                print("No information available.")
+            
+
+student_records()
